9566a8060bf9efeed6c0bfce48d41a70392e501e
azavrin <zavrinlex@gmail.com>
2023-11-12 17:44:14 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/scroll-app",
+    base: "/scroll-app/",
     plugins: [react()],
     server: {
         open: true,
